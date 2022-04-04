6c8a71f1bcb0acf4d98a6c5ab6e08b776f0babae
Sakshi Shetty <sakshishetty@Sakshis-MacBook-Pro.local>
2022-04-04 11:26:14 +0530

    To create a string

---
+++ sequences.py
@@ -0,0 +1,13 @@
+#Creation of String
+
+s="Welcome to Python learnings"
+print(s)
+
+#index returns the value at 2
+print(s[2])
+
+#to print same string multiple times
+print(s*3)
+
+#To gvive length of thye string
+print(len(s))
