22f2abf1f21cefbb4d26f0b2cd14c55ed4a5e43e
Sakshi Shetty <sakshishetty@Sakshis-MacBook-Pro.local>
2022-04-05 10:16:21 +0530

    Use of Tuple and its function

---
+++ tuple.py
@@ -0,0 +1,12 @@
+tpl = (23, 56, 13, 20)
+print(tpl)
+print(tpl * 3)
+
+print(tpl.count(23))
+
+print(tpl.index(13))
+
+# Conversion of list to tuple
+lst = [1, 2, 3, 5, "xyz"]
+tup1=tuple(lst)
+print(tup1)
