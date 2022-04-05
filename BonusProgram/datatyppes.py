22052ea44dbcccf892108b54625f55b2021897f3
Sakshi Shetty <sakshishetty@Sakshis-MacBook-Pro.local>
2022-04-05 11:03:48 +0530

    Datatypes in python

---
+++ BonusProgram/datatyppes.py
@@ -0,0 +1,10 @@
+id=1
+firstName="John"
+lastName="    Teren"
+hasInsurance=True
+billingAmount=1000.00
+
+print(id, firstName, lastName.lstrip(), hasInsurance, billingAmount)
+print(type(id))
+print(type(hasInsurance))
+print(type(billingAmount))
