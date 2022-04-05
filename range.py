d57e1834fdcb0e4f510b304ca2141a22ca6596d6
Sakshi Shetty <sakshishetty@Sakshis-MacBook-Pro.local>
2022-04-05 10:33:05 +0530

    Use of Range and its function

---
+++ range.py
@@ -0,0 +1,15 @@
+r = range(5)
+
+"""For loop -> each element in r, print each of the element"""
+for i in r:
+    print(i)
+
+# Excludes the last number 10
+w = range(1, 10)
+for j in w:
+    print(j)
+
+# Skip numbers using step
+q = range(1, 9, 2)
+for k in q:
+    print(k)
