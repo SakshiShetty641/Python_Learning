580af9883137df7fa37a1de3ae6af2a90025d76f
Sakshi Shetty <sakshishetty@Sakshis-MacBook-Pro.local>
2022-04-24 16:26:47 +0530

    Operators in python

--- operators.py
+++ operators.py
@@ -1,3 +1,4 @@
+# Arithmetic Operators
 a, b = 10, 5
 print("Addition is : ", a + b)
 print("Subtraction is : ", a - b)
@@ -6,3 +7,15 @@ print("Division is : ", a / b)
 print("Mod is : ", a % b)
 print("Exponent is : ", a ** b)
 print("Floor div also know as integer division: ", a // b)
+
+# Assignment Operators
+x = y = z = 10
+print(x, y, z)
+
+# Comparison Operators ==, !=, <, >, <=, >=
+w = 10
+s = 98
+print(w == s)
+print(w != s)
+print(w <= s)
+print(w >= s)
