ab000f5c7ee1040138bdc7c6232f3de2d0606bc4
Sakshi Shetty <sakshishetty@Sakshis-MacBook-Pro.local>
2022-04-24 16:11:25 +0530

    To declare arithimatic operators

---
+++ operators.py
@@ -0,0 +1,8 @@
+a, b = 10, 5
+print("Addition is : ", a + b)
+print("Subtraction is : ", a - b)
+print("Multiplication is : ", a * b)
+print("Division is : ", a / b)
+print("Mod is : ", a % b)
+print("Exponent is : ", a ** b)
+print("Floor div also know as integer division: ", a // b)
